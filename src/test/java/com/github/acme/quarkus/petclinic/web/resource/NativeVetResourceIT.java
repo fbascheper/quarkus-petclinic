d565e20371471b7db192c5e5fbab9b7c985b5aa7
Erik-Berndt Scheper <e.scheper@justid.nl>
2019-08-28 11:31:34 +0200

    mvn io.quarkus:quarkus-maven-plugin:0.21.1:create -DprojectGroupId=com.github.acme -DprojectArtifactId=quarkus-petclinic -DclassName="com.github.acme.quarkus.petclinic.web.resource.VetResource" -Dpath="/vets"

---
+++ src/test/java/com/github/acme/quarkus/petclinic/web/resource/NativeVetResourceIT.java
@@ -0,0 +1,9 @@
+package com.github.acme.quarkus.petclinic.web.resource;
+
+import io.quarkus.test.junit.SubstrateTest;
+
+@SubstrateTest
+public class NativeVetResourceIT extends VetResourceTest {
+
+    // Execute the same tests but in native mode.
+}
